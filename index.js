599c9a1001c22678a5c7ebc8cc910a22e100c9e5
Ben Dow <benjamindow1@gmail.com>
2021-11-16 13:47:40 -0500

    port fix

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
 
 app.use(express.static('static'));
 
@@ -8,4 +7,4 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Server is running`));
